d3a63251e8bf5ef1f453db7777e6ff56fc020b98
Daniel <jdvela158@gmail.com>
2025-06-30 18:12:59 -0600

    Change window location

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -22,7 +22,7 @@ export const AuthProvider = ({ children }) => {
                 const storedUser = localStorage.getItem('user');
                 if (storedUser) {
                     setUser(JSON.parse(storedUser));
-                    navigate('/app/home');
+                    navigate(ROUTES.HOME);
                 }
             } catch (error) {
                 console.error("Error al obtener usuarios ", error);
@@ -49,7 +49,7 @@ export const AuthProvider = ({ children }) => {
             }
             setUser(matchedUser);
             localStorage.setItem("user", JSON.stringify(loggedUser));
-            navigate('/app/home');
+            navigate(ROUTES.HOME);
         } else {
             alert("Usuario o contraseña incorrectos");
         }
